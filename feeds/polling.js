be743820ee3d3bc1579e5e798fd51b984261c216
khf01 <khaleel.freeman@sky.uk>
2019-12-25 21:57:23 +0000

    remove stream id const

--- feeds/polling.js
+++ feeds/polling.js
@@ -4,7 +4,6 @@ const email = require('./util/email-reminder');
 const {writeFile} = require('./util/writeToFile');
 
 const THIRTY_MIN = 1.8e6;
-const STREAM_ID = 'user/a9d30e18-47ec-4606-b3b8-46aa2c138647/category/96e0529d-bdbd-4e15-b75b-48ecc0f3c3a2'
 
 module.exports = {
     liverpoolFeeds: function () {
@@ -14,7 +13,7 @@ module.exports = {
                 {
                     headers: { [process.env.AUTH_KEY]: process.env.AUTH_VALUE },
                     qs: {
-                        streamId: STREAM_ID,
+                        streamId: process.env.STREAM_ID,
                         count: 3,
                         hours: 9,
                     }
@@ -37,7 +36,7 @@ module.exports = {
                 {
                     headers: { [process.env.AUTH_KEY]: process.env.AUTH_VALUE },
                     qs: {
-                        streamId: STREAM_ID,
+                        streamId: process.env.STREAM_ID,
                         count: 40,
                         ranked: 'newest',
                         similar: true,
