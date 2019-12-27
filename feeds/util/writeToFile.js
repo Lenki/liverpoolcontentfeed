6d15b727c8df0eef27ae65fe16c9fc4ca6c292fc
khf01 <khaleel.freeman@sky.uk>
2019-12-27 13:32:24 +0000

    update log

--- feeds/util/writeToFile.js
+++ feeds/util/writeToFile.js
@@ -3,6 +3,7 @@ const writeToFile = (path, data) => {
     fs.writeFile(path, JSON.stringify(data), (err) => {
         if (err) {
             console.log(`error writing to ${path}`);
+            console.log(err);
         }
         else {
             console.log(`succesfully wrote to ${path}`);
